21cb45f5796fa16acfa59e660c62855b98651596
Alexandre Courbot <acourbot@nvidia.com>
2014-02-24 16:07:44 +0900

    shield: mmc: [HACK] sdhci fix for wifi
    https://www.mail-archive.com/linux-mmc@vger.kernel.org/msg23985.html

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1365,7 +1365,8 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 		 * tuning procedure before sending command.
 		 */
 		if ((host->flags & SDHCI_NEEDS_RETUNING) &&
-		    !(present_state & (SDHCI_DOING_WRITE | SDHCI_DOING_READ))) {
+		    !(present_state & (SDHCI_DOING_WRITE | SDHCI_DOING_READ)) &&
+		    mmc_cmd_type(mrq->cmd) == MMC_CMD_ADTC) {
 			if (mmc->card) {
 				/* eMMC uses cmd21 but sd and sdio use cmd19 */
 				tuning_opcode =
